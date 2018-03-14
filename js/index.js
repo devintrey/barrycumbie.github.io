1385d25d64c8eaa884ff0916fe0a65a82c0eb438
devintrey <37382083+devintrey@users.noreply.github.com>
2018-03-14 14:22:16 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 //javascript to print dom
 
-var allDOMEElements = document;
-console.log(allDOMElements);
+$(document).ready(function() {
+    var indexItems = document.getElementsByTagName("*");
+    for (var i=0, max=indexItems.length; i < max; i++) {
+        console.log(indexItems[i].tagName);
+    }
+                  
+ //var allDOMEElements = document;
+    //console.log(allDOMElements);
 
-function printDOM() {
-    var x = document.getElementById("myOutput").name;
-    document.getElementById("demo").innerHTML = x;
+//function printDOM() {
+    //var x = document.getElementById("myOutput").name;
+    //document.getElementById("demo").innerHTML = x;
     //console.log(document.getElementsByTagName('body')[0].innerHTML);
+    
+    
+});
