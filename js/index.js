e831d54986f6745611d71c7f8055899b117c9fff
devintrey <37382083+devintrey@users.noreply.github.com>
2018-03-14 14:44:59 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     var indexItems = document.getElementsByTagName("*");
-    for (var i=0, max=indexItems.length; i < max; i++) {
+    for (var i=0; i < indexItems.length; i++) {
         console.log(indexItems[i].tagName);
     }
                   
