8d2f9d5862c17b70bf6f1e3bf126e2bcaa80937d
devintrey <37382083+devintrey@users.noreply.github.com>
2018-03-14 14:04:59 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//javascript to print dom
+
+var allDOMEElements = document;
+console.log(allDOMElements);
+
+function printDOM() {
+    var x = document.getElementById("myOutput").name;
+    document.getElementById("demo").innerHTML = x;
+    //console.log(document.getElementsByTagName('body')[0].innerHTML);
